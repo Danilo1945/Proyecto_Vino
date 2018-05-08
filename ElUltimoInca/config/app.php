1d2809b900e8f390d6ef27db65455e5597daf5a6
dmoyaUTC <danilomoya19@gmail.com>
2018-05-07 20:31:46 -0500

    primer pull v1.0

--- ElUltimoInca/config/app.php
+++ ElUltimoInca/config/app.php
@@ -222,8 +222,6 @@ return [
             'className' => 'Cake\Database\Connection',
             'driver' => 'Cake\Database\Driver\Mysql',
             'persistent' => false,
-<<<<<<< HEAD
-
 
 
             'host' => 'localhost:3307',
@@ -231,10 +229,6 @@ return [
          
        
 
-            
-=======
-            'host' => 'localhost:3306',
->>>>>>> master
             /**
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
@@ -243,11 +237,9 @@ return [
             //'port' => 'non_standard_port_number',
             'username' => 'root',
             'password' => '',
-<<<<<<< HEAD
-            'database' => 'elultimoinca',
-=======
+
             'database' => 'elulitmoinca',
->>>>>>> master
+
             'encoding' => 'utf8',
             'timezone' => 'UTC',
             'flags' => [],
